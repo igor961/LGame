13a37ddb1f477738d968bcbef27398ffc9ef7c57
Igor <igor.shesterikov2017@gmail.com>
2020-10-25 16:08:25 +0200

    add build-web

--- build-web.sh
+++ build-web.sh
@@ -8,4 +8,5 @@ make PLATFORM=PLATFORM_WEB -B
 
 cd ~/project
 
-make PLATFORM=PLATFORM_WEB -B
+mkdir -p builds/web
+emcc -o builds/web/game.html src/*.c -I ../raylib/src -L ../raylib/src -static -lraylib -lglfw -s USE_GLFW=3 -s FORCE_FILESYSTEM=1 -s ASYNCIFY -s TOTAL_MEMORY=67108864 --shell-file ../raylib/src/shell.html --preload-file assets
